4628f2dd4bb74c324abdd31b473b2a63e6c95297
xingguang2024 <xingguang@clare.ai>
2025-08-17 00:48:29 +0800

    feat: 支持 http 请求

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/cheggaaa/pb/v3 v3.1.7
 	github.com/dop251/goja v0.0.0-20250624190929-4d26883d182a
 	github.com/fatih/color v1.18.0
+	github.com/gin-gonic/gin v1.10.1
 	github.com/gocolly/colly/v2 v2.2.0
 	github.com/itchyny/gojq v0.12.17
 	github.com/json-iterator/go v1.1.12
@@ -28,32 +29,50 @@ require (
 	github.com/antchfx/xpath v1.3.4 // indirect
 	github.com/bitly/go-simplejson v0.5.1 // indirect
 	github.com/bits-and-blooms/bitset v1.22.0 // indirect
+	github.com/bytedance/sonic v1.11.6 // indirect
+	github.com/bytedance/sonic/loader v0.1.1 // indirect
+	github.com/cloudwego/base64x v0.1.4 // indirect
+	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.7 // indirect
 	github.com/dlclark/regexp2 v1.11.5 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/go-sourcemap/sourcemap v2.1.4+incompatible // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/pprof v0.0.0-20250629210550-e611ec304b22 // indirect
 	github.com/itchyny/timefmt-go v0.1.6 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/kr/text v0.2.0 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/nlnwa/whatwg-url v0.6.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.12 // indirect
 	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
+	golang.org/x/arch v0.8.0 // indirect
+	golang.org/x/crypto v0.39.0 // indirect
 	golang.org/x/net v0.41.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.26.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
 	gopkg.in/sourcemap.v1 v1.0.5 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,21 +1,83 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"net/http"
 
-	"github.com/fatih/color"
-
-	"github.com/iawia002/lux/app"
+	"github.com/gin-gonic/gin"
+	"github.com/iawia002/lux/extractors"
+	"github.com/iawia002/lux/extractors/bilibili"
+	"github.com/iawia002/lux/extractors/douyin"
+	"github.com/iawia002/lux/extractors/douyu"
+	"github.com/iawia002/lux/extractors/eporner"
+	"github.com/iawia002/lux/extractors/facebook"
+	"github.com/iawia002/lux/extractors/haokan"
+	"github.com/iawia002/lux/extractors/huya"
+	"github.com/iawia002/lux/extractors/instagram"
+	"github.com/iawia002/lux/extractors/kuaishou"
+	"github.com/iawia002/lux/extractors/mgtv"
+	"github.com/iawia002/lux/extractors/netease"
+	"github.com/iawia002/lux/extractors/pinterest"
+	"github.com/iawia002/lux/extractors/pixivision"
+	"github.com/iawia002/lux/extractors/pornhub"
+	"github.com/iawia002/lux/extractors/qq"
+	"github.com/iawia002/lux/extractors/tiktok"
+	"github.com/iawia002/lux/extractors/tumblr"
+	"github.com/iawia002/lux/extractors/twitter"
+	"github.com/iawia002/lux/extractors/universal"
+	"github.com/iawia002/lux/extractors/weibo"
+	"github.com/iawia002/lux/extractors/xiaohongshu"
+	"github.com/iawia002/lux/extractors/xvideos"
+	"github.com/iawia002/lux/extractors/youtube"
+	"github.com/iawia002/lux/extractors/youku"
 )
 
 func main() {
-	if err := app.New().Run(os.Args); err != nil {
-		fmt.Fprintf(
-			color.Output,
-			"Run %s failed: %s\n",
-			color.CyanString("%s", app.Name), color.RedString("%v", err),
-		)
-		os.Exit(1)
+	extractors.Register("bilibili", bilibili.New())
+	extractors.Register("douyin", douyin.New())
+	extractors.Register("douyu", douyu.New())
+	extractors.Register("eporner", eporner.New())
+	extractors.Register("facebook", facebook.New())
+	extractors.Register("haokan", haokan.New())
+	extractors.Register("huya", huya.New())
+	extractors.Register("instagram", instagram.New())
+	extractors.Register("kuaishou", kuaishou.New())
+	extractors.Register("mgtv", mgtv.New())
+	extractors.Register("netease", netease.New())
+	extractors.Register("pinterest", pinterest.New())
+	extractors.Register("pixivision", pixivision.New())
+	extractors.Register("pornhub", pornhub.New())
+	extractors.Register("qq", qq.New())
+	extractors.Register("tiktok", tiktok.New())
+	extractors.Register("tumblr", tumblr.New())
+	extractors.Register("twitter", twitter.New())
+	extractors.Register("universal", universal.New())
+	extractors.Register("weibo", weibo.New())
+	extractors.Register("xiaohongshu", xiaohongshu.New())
+	extractors.Register("xvideos", xvideos.New())
+	extractors.Register("youtube", youtube.New())
+	extractors.Register("youku", youku.New())
+	r := gin.Default()
+	api := r.Group("/api")
+	{
+		api.GET("/extract", func(c *gin.Context) {
+			url := c.Query("url")
+			if url == "" {
+				c.JSON(http.StatusBadRequest, gin.H{
+					"error": "url is required",
+				})
+				return
+			}
+
+			data, err := extractors.Extract(url, extractors.Options{})
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"error": err.Error(),
+				})
+				return
+			}
+			c.JSON(http.StatusOK, data)
+		})
 	}
+
+	r.Run(":8080") // listen and serve on 0.0.0.0:8080
 }
