4d5ba36b5c3814da8971c638244ed08240c287a3
xingguang2024 <xingguang@clare.ai>
2025-08-17 13:26:52 +0800

    fix: vercel router

--- api/extract/index.go
+++ api/extract/index.go
@@ -3,10 +3,65 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
+	"sync"
 
 	"github.com/iawia002/lux/extractors"
+	"github.com/iawia002/lux/extractors/bilibili"
+	"github.com/iawia002/lux/extractors/douyin"
+	"github.com/iawia002/lux/extractors/douyu"
+	"github.com/iawia002/lux/extractors/eporner"
+	"github.com/iawia002/lux/extractors/facebook"
+	"github.com/iawia002/lux/extractors/haokan"
+	"github.com/iawia002/lux/extractors/huya"
+	"github.com/iawia002/lux/extractors/instagram"
+	"github.com/iawia002/lux/extractors/kuaishou"
+	"github.com/iawia002/lux/extractors/mgtv"
+	"github.com/iawia002/lux/extractors/netease"
+	"github.com/iawia002/lux/extractors/pinterest"
+	"github.com/iawia002/lux/extractors/pixivision"
+	"github.com/iawia002/lux/extractors/pornhub"
+	"github.com/iawia002/lux/extractors/qq"
+	"github.com/iawia002/lux/extractors/tiktok"
+	"github.com/iawia002/lux/extractors/tumblr"
+	"github.com/iawia002/lux/extractors/twitter"
+	"github.com/iawia002/lux/extractors/universal"
+	"github.com/iawia002/lux/extractors/weibo"
+	"github.com/iawia002/lux/extractors/xiaohongshu"
+	"github.com/iawia002/lux/extractors/xvideos"
+	"github.com/iawia002/lux/extractors/youtube"
+	"github.com/iawia002/lux/extractors/youku"
 	"github.com/iawia002/lux/utils"
 )
+
+var once sync.Once
+
+// Initialize extractors once
+func initializeExtractors() {
+	extractors.Register("bilibili", bilibili.New())
+	extractors.Register("douyin", douyin.New())
+	extractors.Register("douyu", douyu.New())
+	extractors.Register("eporner", eporner.New())
+	extractors.Register("facebook", facebook.New())
+	extractors.Register("haokan", haokan.New())
+	extractors.Register("huya", huya.New())
+	extractors.Register("instagram", instagram.New())
+	extractors.Register("kuaishou", kuaishou.New())
+	extractors.Register("mgtv", mgtv.New())
+	extractors.Register("netease", netease.New())
+	extractors.Register("pinterest", pinterest.New())
+	extractors.Register("pixivision", pixivision.New())
+	extractors.Register("pornhub", pornhub.New())
+	extractors.Register("qq", qq.New())
+	extractors.Register("tiktok", tiktok.New())
+	extractors.Register("tumblr", tumblr.New())
+	extractors.Register("twitter", twitter.New())
+	extractors.Register("universal", universal.New())
+	extractors.Register("weibo", weibo.New())
+	extractors.Register("xiaohongshu", xiaohongshu.New())
+	extractors.Register("xvideos", xvideos.New())
+	extractors.Register("youtube", youtube.New())
+	extractors.Register("youku", youku.New())
+}
 
 // Helper function for JSON responses
 func writeJSON(w http.ResponseWriter, statusCode int, data interface{}) {
@@ -17,6 +72,9 @@ func writeJSON(w http.ResponseWriter, statusCode int, data interface{}) {
 
 // Handler for the /api/extract endpoint
 func Handler(w http.ResponseWriter, r *http.Request) {
+	// Initialize extractors once
+	once.Do(initializeExtractors)
+	
 	if r.Method != http.MethodPost {
 		writeJSON(w, http.StatusMethodNotAllowed, map[string]string{"error": "Method not allowed"})
 		return
